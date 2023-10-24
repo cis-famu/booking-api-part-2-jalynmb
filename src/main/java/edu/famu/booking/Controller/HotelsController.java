93d452a00f5358384b2bd10c12beb722eef6f218
jalynmb <jalyn1.baker@famu.edu>
2023-10-24 18:26:01 -0400

    Update ReadME and Hotel Controller

--- src/main/java/edu/famu/booking/Controller/HotelsController.java
+++ src/main/java/edu/famu/booking/Controller/HotelsController.java
@@ -37,4 +37,56 @@ public class HotelsController {
             return ResponseEntity.status(500).body(new ApiResponse(false, "An error occurred.", null, e.getMessage()));
         }
     }
+
+    @PostMapping("/")
+    public ResponseEntity<Map<String,Object>> createHotel(@RequestBody Hotels hotel){
+        try{
+            payload = hotelsService.createHotel(hotel);
+            statusCode = 201;
+            name = "hotelId";
+        } catch (ExecutionException | InterruptedException e) {
+            payload = new ErrorMessage("Cannot create new hotel in database.", CLASS_NAME, e.toString());
+        }
+
+        response = new ResponseWrapper(statusCode,name, payload);
+
+        return response.getResponse();
+    }
+
+    @PutMapping("/{hotelId}")
+    public ResponseEntity<Map<String,Object>> updatePost(@PathVariable(name="hotelId") String id, @RequestBody Map<String, Object> updateValues){
+        try{
+
+            hotelsService.updateHotel(id, updateValues);
+            statusCode = 201;
+            name = "message";
+            payload = "Update successful for hotel with id " + id;
+
+        }catch (ParseException e){
+            statusCode = 400;
+            payload = new ErrorMessage("Cannot parse JSON",CLASS_NAME, e.toString());
+        }
+        catch (Exception e) {
+            payload = new ErrorMessage("Cannot update hotel with id " + id,CLASS_NAME, e.toString());
+        }
+
+        response = new ResponseWrapper(statusCode,name, payload);
+
+        return response.getResponse();
+    }
+
+    @DeleteMapping("/{hotelId}")
+    public ResponseEntity<Map<String,Object>> removePost(@PathVariable(name="hotelId") String id){
+        try{
+            hotelsService.deleteHotel(id);
+            statusCode = 204;
+            name = "message";
+            payload = "Delete successful for hotel with id " + id;
+        }catch (Exception e){
+            payload = new ErrorMessage("Cannot delete hotel with id " + id, CLASS_NAME, e.toString());
+        }
+        response = new ResponseWrapper(statusCode,name, payload);
+
+        return response.getResponse();
+    }
 }
